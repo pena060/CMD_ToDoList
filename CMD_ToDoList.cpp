8f4b100e587e0dbfe0d3fceca7d59e964a71bea7
Cristian Pena <61167020+pena060@users.noreply.github.com>
2022-03-15 14:24:19 -0700

    persistent list trhough fstream

--- CMD_ToDoList.cpp
+++ CMD_ToDoList.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <stdlib.h>
+#include <fstream>
 
 using std::cout;
 
@@ -16,22 +17,34 @@ void RemoveFromList(std::vector<std::string>& List);
 int main()
 {
     std::vector<std::string> List;
-
+    std::string str;
     int input;
+
+    std::ifstream persistentList("List.txt");
+    if (!persistentList.fail()) {
+        while (std::getline(persistentList, str))
+        {
+          
+            if (str.size() > 0)
+                List.push_back(str);
+        }
+    }
+
+    persistentList.close();
     
 
-        cout << "Welcome to your to-do list!\nv0.9b\n\n";
+    cout << "Welcome to your to-do list!\nv0.9b\n\n";
 
-        cout << "What would you like to do? \n";
-        cout << "\t" << "1. View List \n";
-        cout << "\t" << "2. Add item to list \n";
-        cout << "\t" << "3. Remove item from list \n";
-        cout << "\t" << "4. Exit \n";
-        cout << ">> ";
-        std::cin >> input;
-        cout << "\n";
+    cout << "What would you like to do? \n";
+    cout << "\t" << "1. View List \n";
+    cout << "\t" << "2. Add item to list \n";
+    cout << "\t" << "3. Remove item from list \n";
+    cout << "\t" << "4. Exit \n";
+    cout << ">> ";
+    std::cin >> input;
+    cout << "\n";
 
-        while (input != 4){
+    while (input != 4){
 
         switch (input) {
         case 1:
@@ -53,36 +66,59 @@ int main()
         cout << "\n";
     } 
 
+
+    std::system("cls");
+    cout << " >> Program closed successfully...Goodbye \n";
+
+        
+    return 0;
 }
 
 
 void RemoveFromList(std::vector<std::string>& List){
+    std::fstream File("List.txt", std::ios::out | std::ios::trunc);
 
-    int selection;
+    if (!File.fail()){
 
-    if (List.size() > 0) {
+        int selection;
         cout << "Which item would you like to delete (type index number) \n";
         cout << ">> ";
         std::cin >> selection;
         cout << "\n";
 
 
-        List.erase(List.begin() + (selection - 1)); 
-        cout << "Item successfully Removed! Returning to menu...\n\n";
+        if (selection < 0 || selection >= List.size()) {
+            cout << "*ERROR* index out of scope, select an item index from within the list!\n\n";
 
+        }
+        else {
+
+            List.erase(List.begin() + selection);
+
+            if (List.size() > 0){
+                for (int i = 0; i < List.size(); i++) {
+                    File << List[i] << "\n";
+                }
+                File.close();
+            }else{
+                File.close();
+                std::remove("List.txt");
+            }
+            
+            cout << "Item successfully Removed! Returning to menu...\n\n";
+
+        }
+
+    }else {
+
+        cout << "*ERROR* List is empty! (Add one item to create a list before trying to remove it) \n\n";
     }
-    else {
-
-        cout << "List is empty! Returning to menu... \n\n";
-    }
-
-
-
-
-
 }
 
 void AddToList(std::vector<std::string>& List){
+    std::ofstream oFile;
+    oFile.open("List.txt", std::ios::app);
+
     std::string newItem;
     std::cin.clear();
     std::cin.ignore(10000, '\n');
@@ -94,15 +130,28 @@ void AddToList(std::vector<std::string>& List){
     cout << "\n";
     List.push_back(newItem);
 
+    oFile << newItem << "\n";
+
+    oFile.close();
+
     cout << "Item successfully Added! Returning to menu...\n\n";
 
 }
 
 void ShowList(std::vector<std::string>& List){
-    cout << "TO-DO List: \n";
-    for (int i = 0; i < List.size(); i++){
-        cout << "\t" << i + 1 << ". " << List[i] << "\n";
+    std::ifstream inFile;
+    inFile.open("List.txt");
+
+    if (inFile.is_open()) {
+        cout << "TO-DO List: \n";
+
+        cout << inFile.rdbuf() << "\n";
+
+        inFile.close();
+        cout << "End of list.....\n\n";
     }
-    cout << "End of list.....\n\n";
+    else {
+        cout << "*ERROR* List is empty! (Add one item to create a list before trying to view list) \n\n";
+    }
 }
 
